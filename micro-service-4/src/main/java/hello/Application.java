09fe8789cb3cd56b4eef9626e392a82c684ce1af
ranjeetkgupta <guptar20@INGSGUPTAR20L3C.corp.emc.com>
2016-02-11 17:01:00 +0530

    added few more ms

---
+++ micro-service-4/src/main/java/hello/Application.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		Date date = new Date();
+		System.out.println("Microservice 4 invoked !!" + date.toString());
+		return "Hello Docker World  -- ms4";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
