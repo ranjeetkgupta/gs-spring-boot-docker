0538febea0927862b014951100dbb2ae8fba10fa
ranjeetkgupta <guptar20@INGSGUPTAR20L3C.corp.emc.com>
2016-02-11 11:36:35 +0530

    changed ports

--- micro-service-1/src/main/java/hello/Application.java
+++ micro-service-1/src/main/java/hello/Application.java
@@ -1,21 +1,47 @@
 package hello;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Date;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	@RequestMapping("/")
+	
+    @Value("${db.pool.size}")
+    String poolSize;
+    
+    @Value("${db.url}")
+    String dbUrl;
+    
+    @Value("${ms2.url}")
+    String ms2Url;
+    
+    @Value("${rabbit.ip}")
+    String rabbitIp;
+    
+    
+    @RequestMapping("/")
 	public String home() {
 		Date date = new Date();
 		System.out.println("Microservice 1 invoked !!" + date.toString());
-		return "Hello Docker -- from ms 1";
+		
+		StringBuilder builder =  new StringBuilder();
+		
+		builder.append("Hello Docker -- from microservice 1 \n");
+		builder.append("env variable :: \n");
+		builder.append("poolSize >> " + poolSize + "\n" + "dbUrl >> " + dbUrl + "\n" + "ms2url >> " + ms2Url + "\n" + "rabbitip >> " + rabbitIp);
+		builder.append("Response from urilty microservice >>" + invokeUtilityService(ms2Url));
+		
+		return builder.toString();
 	}
 
 
@@ -23,4 +49,19 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	
+	String invokeUtilityService(String url)
+	{
+	   
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	    String result = restTemplate.getForObject(url, String.class);
+	     
+	    System.out.println(result);
+	    
+	    return result;
+		
+	}
+	
+	
 }
